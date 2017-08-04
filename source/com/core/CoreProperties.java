dc23777a1cccd411ccfc07eba53b8b625d56695d
Maruthamuthu.G <maruthamuthu.g@zohocorp.com>
2017-08-04 21:45:19 +0530

    Change CoreProperties class

--- source/com/core/CoreProperties.java
+++ source/com/core/CoreProperties.java
@@ -1,79 +1,28 @@
 package com.core;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 /**
  * @author Maruthamuthu
- *         CoreProperties is a singleton class.
  *         This is used to initialize the properties for the whole project
  */
 
-public class CoreProperties extends Properties
+public class CoreProperties
 {
-	private static volatile CoreProperties coreProperties;
+	private static Properties coreProperties = new Properties();
 
-	/*
-		This constructor validates whether coreProperties is initialized or not. If already initialized then error thrown.
-    	This check needed to keep this class is singleton and can't violate singleton using java reflection.
- 	*/
-	private CoreProperties() throws CoreException, IOException
+	public static String get(String key)
 	{
-		if(coreProperties != null)
-		{
-			throw new CoreException("CoreProperties is a singleton class, so use getInstance() instead of new CoreProperties()");
-		}
-		checkAndLoadProperties();
+		return coreProperties.getProperty(key);
 	}
 
-	/*
-		The getInstance() is used to get the sole instance of CoreProperties class.
-	 */
-	public static CoreProperties getInstance() throws Exception
+	public static String get(String key, String defaultValue)
 	{
-		/* Checks coreProperties is already initialized or not*/
-		if(coreProperties == null)
-		{
-			/* Getting lock for CoreProperties class*/
-			synchronized(CoreProperties.class)
-			{
-				/* If one thread initializing the coreProperties and another one waiting for lock (both threads passed the first if check) then we need to
-				* check null check again */
-
-				if(coreProperties == null)
-				{
-					coreProperties = new CoreProperties();
-				}
-			}
-		}
-		return coreProperties;
+		return coreProperties.getProperty(key, defaultValue);
 	}
 
-	/*
-		The checkAndLoadProperties() is used to load the properties from core.properties file
-		The core.properties is not mandatory, so that the file exists check added.
-	 */
-	private void checkAndLoadProperties() throws CoreException, IOException
+	public static Object set(String key, String value)
 	{
-		File propertyFile = new File("core.properties");
-		if(propertyFile.exists())
-		{
-			InputStream propStream = null;
-			try
-			{
-				propStream = new FileInputStream(propertyFile);
-				this.load(propStream);
-			}
-			finally
-			{
-				if(propStream != null)
-				{
-					propStream.close();
-				}
-			}
-		}
+		return coreProperties.setProperty(key, value);
 	}
 }
